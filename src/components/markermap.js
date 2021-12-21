ab538d3a1ec9b332b1b0c242812ae2b4cfc5da11
Venus9023 <90791245+Venus9023@users.noreply.github.com>
2021-12-21 03:13:32 -0800

    fix bug

--- src/components/markermap.js
+++ src/components/markermap.js
@@ -10,7 +10,6 @@ import { useNavigate } from "react-router-dom";
 import { makeStyles, withStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
 import { SitesContext } from "../contexts/sites";
-import CustomizedInputBase from './searchbox';
 
 const useStyles = makeStyles({
   root: {
