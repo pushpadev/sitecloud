cc75f0208a6265ff17f76d2571523aca955d28f2
priya lakshmi <lekhapriya98@gmail.com>
2021-12-26 15:54:17 +0530

    home page mappin info fixed the overflow

--- src/components/mappin.js
+++ src/components/mappin.js
@@ -8,30 +8,30 @@ import { BG_COLOR_WHITE } from '../constant';
 const useStyles = makeStyles((theme) =>
   createStyles({
     messageBlue: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        position: "relative",
-        marginLeft: "20px",
-        marginBottom: "10px",
-        padding: "10px",
-        backgroundColor: "#0066ffef",
-        color: 'white',
-        width: 160,
-        height: 40,
-        textAlign: "left",
-        font: "400 .9em 'Open Sans', sans-serif",
-        border: "1px solid #0066ffef",
-        "&:after": {
-            content: "''",
-            position: "absolute",
-            width: "0",
-            height: "0",
-            borderTop: "25px solid #0066ffef",
-            borderLeft: "15px solid transparent",
-            bottom: '-25px',
-            right: '-1px'
-        },
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      position: "relative",
+      marginLeft: "20px",
+      marginBottom: "10px",
+      padding: "10px",
+      backgroundColor: "#0066ffef",
+      color: 'white',
+      width: 160,
+      height: 40,
+      textAlign: "left",
+      font: "400 .9em 'Open Sans', sans-serif",
+      border: "1px solid #0066ffef",
+      "&:after": {
+        content: "''",
+        position: "absolute",
+        width: "0",
+        height: "0",
+        borderTop: "25px solid #0066ffef",
+        borderLeft: "15px solid transparent",
+        bottom: '-25px',
+        right: '-1px'
+      },
     },
     messageWhite: {
       display: 'flex',
@@ -48,25 +48,33 @@ const useStyles = makeStyles((theme) =>
       font: "400 .9em 'Open Sans', sans-serif",
       border: "1px solid white",
       "&:after": {
-          content: "''",
-          position: "absolute",
-          width: "0",
-          height: "0",
-          borderTop: "25px solid white",
-          borderLeft: "15px solid transparent",
-          bottom: '-25px',
-          right: '-1px'
+        content: "''",
+        position: "absolute",
+        width: "0",
+        height: "0",
+        borderTop: "25px solid white",
+        borderLeft: "15px solid transparent",
+        bottom: '-25px',
+        right: '-1px'
       },
-  },
+    },
     messageContentBlue: {
       padding: 0,
       marginLeft: 10,
       color: BG_COLOR_WHITE,
+      width: 110,
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis'
     },
     messageContentWhite: {
       padding: 0,
       marginLeft: 10,
       color: 'black',
+      width: 110,
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis'
     },
     displayName: {
       marginLeft: "20px"
@@ -74,15 +82,15 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
-export default function MapPin(props){
+export default function MapPin(props) {
   const message = props.message;
   const classes = useStyles();
   return (
     <>
-          <div className={(props.isSelec === true)?classes.messageBlue:classes.messageWhite}>
-            {(props.isSelec === true)?<WhiteMapPinIcon />: <MapPinIcon />}
-            <span className={(props.isSelec === true)?classes.messageContentBlue:classes.messageContentWhite}>{message}</span>
-          </div>
+      <div className={(props.isSelec === true) ? classes.messageBlue : classes.messageWhite}>
+        {(props.isSelec === true) ? <WhiteMapPinIcon /> : <MapPinIcon />}
+        <span className={(props.isSelec === true) ? classes.messageContentBlue : classes.messageContentWhite}>{message}</span>
+      </div>
     </>
   );
 };
