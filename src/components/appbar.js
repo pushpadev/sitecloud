cea2414dc51c085b97edab1441bb8da456e2b1e9
Abinesh <2001mrabi@gmail.com>
2021-12-27 11:24:47 +0530

    logo alignment fixed

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -1,35 +1,47 @@
-import React, { useContext } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import { APP_NAME } from '../constant';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { APP_NAME } from "../constant";
+import { useNavigate } from "react-router-dom";
 
 export default function PrimarySearchAppBar() {
-
   const pathName = window.location.pathname;
   const history = useNavigate();
 
-
   const accountClick = () => {
-    history(`/showsite/accountsetting/`)
-  }
+    history(`/showsite/accountsetting/`);
+  };
 
   return (
     <>
-
       <Box>
-        <AppBar position="static" style={{ backgroundColor: 'white', color: 'black' }}>
+        <AppBar
+          position="static"
+          style={{ backgroundColor: "white", color: "black" }}
+        >
           <Toolbar>
-            <Box sx={{ display: { xs: 'flex', md: 'flex' } }} style={{ alignItems: 'center' }}>
-              <a href='/'><img style={{ width: '40px', marginRight: 5 }} src='/logo.png' alt='logo' /></a>
+            <Box
+              sx={{ display: { xs: "flex", md: "flex" } }}
+              style={{ alignItems: "center" }}
+            >
+              <a href="/">
+                <img
+                  style={{ width: "25px", marginRight: "20px" }}
+                  src="/logo.png"
+                  alt="logo"
+                />
+              </a>
               <span>{APP_NAME}</span>
             </Box>
             <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ display: { xs: 'flex', md: 'flex' } }} style={{ alignItems: 'center' }}>
+            <Box
+              sx={{ display: { xs: "flex", md: "flex" } }}
+              style={{ alignItems: "center" }}
+            >
               <Typography>Account Settings</Typography>
               <IconButton
                 color="primary"
@@ -43,8 +55,6 @@ export default function PrimarySearchAppBar() {
           </Toolbar>
         </AppBar>
       </Box>
-
     </>
-
   );
 }
--- src/components/markermap.js
+++ src/components/markermap.js
@@ -1,52 +1,59 @@
-import React, { useState, useRef, useEffect, useContext, forwardRef, useImperativeHandle } from "react";
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useContext,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
 import ReactMapboxGl, { Marker } from "react-mapbox-gl";
 import DrawControl from "react-mapbox-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-import marker from '../marker.png';
+import marker from "../marker.png";
 import { MAP_VIEW, SIDEBAR_WIDTH, MAP_CENTER_COORDINATE } from "../constant";
-import MapPin from './mappin';
-import PopOver from './popover'
+import MapPin from "./mappin";
+import PopOver from "./popover";
 import { useNavigate } from "react-router-dom";
-import { makeStyles, withStyles } from '@mui/styles';
-import Button from '@mui/material/Button';
+import { makeStyles, withStyles } from "@mui/styles";
+import Button from "@mui/material/Button";
 import { SitesContext } from "../contexts/sites";
 // import SearchBox from './searchbox';
 
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import '../css/style.css';
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import * as MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "../css/style.css";
 
 const useStyles = makeStyles({
   root: {
-      padding: 12,
+    padding: 12,
   },
 });
 
 const ColorButton = withStyles((theme) => ({
   root: {
-      position: 'absolute',
-      top: 100,
-      right: 30,
-      zIndex: 10,
-      width: '140px',
-      height: '35px',
-      color: 'white',
-      paddingTop: '2px',
-      paddingBottom: '2px',
-      textTransform: 'none',
-      borderRadius: 0,
-      backgroundColor: '#0066ffef !important',
-      border: '2px solid #0066ffef',
-      '&:hover': {
-          opacity: '.7',
-          borderColor: '#0066ffef !important',
-      },
+    position: "absolute",
+    top: 100,
+    right: 30,
+    zIndex: 10,
+    width: "140px",
+    height: "35px",
+    color: "white",
+    paddingTop: "2px",
+    paddingBottom: "2px",
+    textTransform: "none",
+    borderRadius: 0,
+    backgroundColor: "#0066ffef !important",
+    border: "2px solid #0066ffef",
+    "&:hover": {
+      opacity: ".7",
+      borderColor: "#0066ffef !important",
+    },
   },
 }))(Button);
 
 const Map = ReactMapboxGl({
   accessToken:
-    "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g"
+    "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g",
 });
 
 const MarkerMap = forwardRef((props, ref) => {
@@ -63,22 +70,23 @@ const MarkerMap = forwardRef((props, ref) => {
 
   const handleClick = (id) => {
     setSiteInfo(sites[id]);
-  }
+  };
 
   const handleDoubleClick = (id) => {
     history(`/editsite/${id}`);
-  }
+  };
   const addNewSite = () => {
-    history('/editsite');
-  }
+    history("/editsite");
+  };
 
-  const onStyleLoaded = (map)  => {
+  const onStyleLoaded = (map) => {
     setMapObj(map);
     const geocoder = new MapboxGeocoder({
       // Initialize the geocoder
-      accessToken: "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g", // Set the access token
+      accessToken:
+        "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g", // Set the access token
       mapboxgl: map, // Set the mapbox-gl instance
-      placeholder: 'Search here', // Placeholder text for the search bar
+      placeholder: "Search here", // Placeholder text for the search bar
       // bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
       // proximity: {
       //   longitude: -122.25948,
@@ -88,31 +96,33 @@ const MarkerMap = forwardRef((props, ref) => {
     map.setZoom(6);
 
     map.addControl(geocoder);
-    if(props.selId === 0){
+    if (props.selId === 0) {
       map.panTo(MAP_CENTER_COORDINATE);
     }
-  }
+  };
 
   const goToSelectedSite = (item) => {
-    if(mapObj){
+    if (mapObj) {
       setSiteInfo(item);
-      if(item.data.centroid)
+      if (item.data.centroid)
         mapObj.panTo([item?.data?.centroid[0], item?.data?.centroid[1]]);
     }
-  }
+  };
   const goToInitialPos = () => {
     setSiteInfo(MAP_VIEW);
-    if(mapObj){
-        mapObj.panTo(MAP_CENTER_COORDINATE);
+    if (mapObj) {
+      mapObj.panTo(MAP_CENTER_COORDINATE);
     }
-  }
+  };
   // [149.012375, -35.473469]
-  useImperativeHandle(ref, () => ({ goToSelectedSite, goToInitialPos }), [mapObj])
+  useImperativeHandle(ref, () => ({ goToSelectedSite, goToInitialPos }), [
+    mapObj,
+  ]);
 
   useEffect(() => {
     setSiteInfo(MAP_VIEW);
-  }, [])
-        // center = {[149.012375, -35.473469]}
+  }, []);
+  // center = {[149.012375, -35.473469]}
 
   return (
     <div className={classes.root}>
@@ -121,46 +131,54 @@ const MarkerMap = forwardRef((props, ref) => {
         onStyleLoad={onStyleLoaded}
         containerStyle={{
           height: `calc(100vh - 84px)`,
-          width: `calc(100vw - ${SIDEBAR_WIDTH}px)`
+          width: `calc(100vw - ${SIDEBAR_WIDTH}px)`,
         }}
         center={MAP_CENTER_COORDINATE}
       >
         <DrawControl ref={drawControl} displayControlsDefault={false} />
-        {(sites.length > 0)? sites.map((item, index) => {
-          return(
-            <Marker
-              key = {index}
-              coordinates={item?.data?.centroid}
-              captureClick={false}
-              draggable={false}
-              offsetTop={-50}
-              offsetLeft={-15}
-              anchor="bottom"
-            >
-                  <div className='custom-pin'
-                    onDoubleClick={() => handleDoubleClick(item.sitemappingId)}
-                    onClick={() => handleClick(index)}
-                  >
-                    <MapPin
-                      isSelec = {(item?.sitemappingId === siteInfo?.sitemappingId)?true:false}
-                      message={item?.data?.Sitename}
-                    />
-                  </div>
-            </Marker>
-          )
-        }):(<></>)}
+        {sites.length > 0 ? (
+          sites.map((item, index) => {
+            return (
+              <Marker
+                key={index}
+                coordinates={item?.data?.centroid}
+                captureClick={false}
+                draggable={false}
+                offsetTop={-50}
+                offsetLeft={-15}
+                anchor="bottom"
+              >
+                <div
+                  className="custom-pin"
+                  onDoubleClick={() => handleDoubleClick(item.sitemappingId)}
+                  onClick={() => handleClick(index)}
+                >
+                  <MapPin
+                    isSelec={
+                      item?.sitemappingId === siteInfo?.sitemappingId
+                        ? true
+                        : false
+                    }
+                    message={item?.data?.Sitename}
+                  />
+                </div>
+              </Marker>
+            );
+          })
+        ) : (
+          <></>
+        )}
       </Map>
-      {(sites.length > 0) && 
-      ( <div style={{display: 'flex', justifyContent: 'center'}}>
-        <PopOver siteInfo = {siteInfo} />
-      </div>)}
-     
-      <ColorButton onClick={() => addNewSite()}>
-        Add New Site
-      </ColorButton>
+      {sites.length > 0 && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <PopOver siteInfo={siteInfo} />
+        </div>
+      )}
+
+      <ColorButton onClick={() => addNewSite()}>Add New Site</ColorButton>
       {/* <div style = {{display: 'flex', justifyContent: 'center'}}><SearchBox /></div> */}
     </div>
   );
-})
+});
 
 export default MarkerMap;
